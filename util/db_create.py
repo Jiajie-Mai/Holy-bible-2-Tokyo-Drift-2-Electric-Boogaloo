1bcce55f87f4d85b1e60a73a09c3cca03b079ceb
Ryan Aday <ryanobelisk@gmail.com>
2019-01-09 12:54:40 -0500

    Modified app.py and db creation files

--- util/db_create.py
+++ util/db_create.py
@@ -4,8 +4,8 @@ DB_FILE = "app.db"
 db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 '''Makes users and posts table'''
-c.execute("CREATE TABLE users (username TEXT, password TEXT)")
-c.execute("CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, body TEXT, time_stamp INTEGER)")
+c.execute("CREATE TABLE users (id INTEGER, username TEXT, password TEXT)")
+c.execute("CREATE TABLE gameData (id INTEGER, money INTEGER, stocksOwned STRING)")
 
 db.commit()
 db.close()
