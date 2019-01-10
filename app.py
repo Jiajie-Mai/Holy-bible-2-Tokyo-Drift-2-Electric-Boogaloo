c167192450c46742c7686c7edd8c87d834b16617
Ryan Aday <ryanobelisk@gmail.com>
2019-01-10 12:58:47 -0500

    checking on app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def home():
 def signup():
     '''Looks for current user; if exists, redirs them to homepage.  Otherwise, sends them to signup page'''
     if "user" in session.keys():
-        return redirect(url_for("index"))
+        return redirect(url_for(""))
     if request.method == "GET":
         return render_template("signup.html", title = "Sign Up", current_user = session.get("user"))
     else:
@@ -29,14 +29,14 @@ def signup():
         hashpass = hash_obj.hexdigest()
         if create_user(username, password):
             login_user(username, hashpass)
-            return redirect(url_for("index"))
+            return redirect(url_for(""))
         return render_template("signup.html", title = "Sign Up")
 
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
     if "user" in session.keys():
-        return redirect(url_for("index"))
+        return redirect(url_for(""))
     if request.method == "GET":
         return render_template("login.html", title = "Login", current_user = session.get("user"))
     else:
@@ -49,7 +49,7 @@ def login():
         '''If login fail, redir to login page, otherwise send user to homepage'''
         if login == None:
             return render_template("login.html", title = "Login")
-        return redirect(url_for("index"))
+        return redirect(url_for(""))
 
 
 @app.route("/logout", methods = ["GET"])
