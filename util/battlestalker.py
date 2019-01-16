12986118d65c0990fe62fe6c97a8c87a5ade448a
Theodore Peters <tpeters@stuy.edu>
2019-01-16 13:00:24 -0500

    ;;;;;;;

--- util/battlestalker.py
+++ util/battlestalker.py
@@ -4,13 +4,15 @@ def reset():
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
     c.execute("DROP TABLE IF EXISTS s;")
-    c.execute("CREATE TABLE s (matchid INTEGER, symbol TEXT, price INTEGER);")
+    c.execute("CREATE TABLE s (matchid INTEGER, symbol TEXT);")
     db.commit()
     db.close()
 
 def add_match(mid):
     db = sqlite3.connect(DB_FILE)
     c = db.cursor
-    c.execute()
+    stocks = ["A", "AA", "AAC", "AAN", "AAP"]
+    for i in stocks:
+        c.execute("")
     db.commit()
     db.close()
